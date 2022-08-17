ca82fae96f7e318bbeab42169ab97b3efbaef0f3
ChristabelAlorzuke <alorzuke2000@gmail.com>
2022-08-17 14:10:37 +0000

    useState

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import {TrashIcon} from '@heroicons/react/outline'
+import React from "react";
+import { TrashIcon } from "@heroicons/react/outline";
 
-function TaskItem({task,handleDelete}){
-    return(
-<div className='flex items-center bg-teal-100 border-2 border-gray-300 rounded-md ' >
-        <div className='flex-1'>{task}</div>
-        <button
-         className='bg-blue-700 p-2 rounded-md'
-         onClick={()=>handleDelete(task)}>
-            <TrashIcon height={24} color="white"/>
-        </button>
-</div>
+function TaskItem({ task, handleDelete }) {
+  return (
+    <div className="flex items-center justify-between bg-teal-100 border-2 border-gray-300 rounded-md ">
       
-       )
+      <div className="flex space-x-2 ">
+        <input type="checkbox" name="" id="" Checked={task.completed===true} onChange={()=>{}} />
+        <div className="flex-1">{task.text}</div>
+      </div>
+      
+      <button
+         className="bg-blue-700 p-2 rounded-md"
+         onClick={() => handleDelete(task.id)}
+      >
+         <TrashIcon height={24} color="yellow" />
+      </button>
+    </div>
+  );
 }
 
-export default TaskItem
+export default TaskItem;
--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,72 +1,82 @@
-import React, { useState } from 'react';
-import TaskItem from './TaskItem';
+import React, { useEffect, useState } from "react";
+import TaskItem from "./TaskItem";
+import { v4 as uuid } from "uuid";
+// import  from '';
+// import  from '';
 // import background from "../assets/ColorArt.jpg";
 // import {list} from "postcss";
 
-
+const getTasksFromlocalStorage = () => {
+  // get the task from the localStorage
+  const savedTasks = localStorage.getItem("tasks");
+  if (!savedTasks) return [];
+  return JSON.parse(savedTasks);
+};
 
 function TaskManager() {
-  const [tasks, setTasks] = useState([]);
-  const [ input, setInput] = useState("");
+  const [tasks, setTasks] = useState(getTasksFromlocalStorage);
+  const [input, setInput] = useState("");
 
-
-  const handleSubmit= (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-//     if(input==="") return;
+    if (input === "") return;
 
-    setTasks([input, ...tasks] , () =>{});
-    setInput("")
+    const newTask = {
+      id: uuid(),
+      text: input,
+      completed: false,
+    };
+
+    setTasks([newTask, ...tasks], () => {});
+    setInput("");
+    localStorage.setItem("tasks", JSON.stringify(tasks));
   };
-// };
-  const handleDelete =idx=> {
-    const newTasks = tasks.filter(task=>task!==idx)
-    setTasks(newTasks)
-  }
- 
 
-   return(
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
+  // };
+  const handleDelete = (id) => {
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
+  };
+
+  return (
     <div className="relative h-screen w-full bg-slate-800/90 ">
-    {/* //    <img src={background}alt="" */}
-    
-        <div className='h-full bg-blue-600 justify-center items-center flex px-5 py-10'>
+      {/* //    <img src={background}alt="" */}
 
-          <div className=' max-w-xl px-5 py-10 bg-white rounded-xl max-h-[500px]'>
+      <div className="h-screen bg-gradient-to-br from-fuchsia-900 justify-center items-center flex px-5 py-10">
+        <div className=" max-w-xl px-16 py-20 bg-white rounded-xl max-h-[500px]">
+          <div className="bg-yellow-800 m-5 mt-0 text-center text-white rounded-lg p-2 outline-none">DAILY REMINDERS</div>
+          <form
+            className="w-full space-x-5 flex justify-between mb-10"
+            onSubmit={handleSubmit}
+          >
+            <input
+              type="text"
+              className="border-2 border-yellow-800 px-16 py-2 rounded-md outline-none w-10/12"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+            />
 
-
-            <form               
-              className="w-full space-x-5 flex justify-between mb-10"
-              onSubmit={handleSubmit}
+            <button
+              type="submit"
+              className="bg-yellow-800 px-5 py-2 text-white text-lg rounded-md"
+              disabled={input === ""}
             >
-
-              <input 
-                type="text" 
-                className='border-2 border-yellow-500 p-2 rounded-md outline-non w-10/12' 
-                onChange={(e) => setInput(e.target.value)}
-                value={input} 
-              />
-
-              <button type='submit'
-                className='bg-purple-600 px-5 py-2 text-white text-lg rounded-md'
-                disabled={input===""}
-                >
-                    Add
-              </button>
+              Add
+            </button>
           </form>
-            <div className='space-y-2 overflow-y-auto h-56'>
-                  {
-                    tasks.map((task) => (
-                      <TaskItem task={task} handleDelete={handleDelete} />))
-                  }
-             </div>      
+          <div className="space-y-2 overflow-y-auto h-56">
+            {tasks.map((task) => (
+              <TaskItem key={task.id} task={task} handleDelete={handleDelete} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-    
-    
-    
-  )
-                }
-                          
+  );
+}
 
-export default TaskManager
+export default TaskManager;
