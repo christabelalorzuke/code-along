e0cbe14b568fc2034161b9bf221ed61e112ee2e6
ChristabelAlorzuke <alorzuke2000@gmail.com>
2022-08-24 14:26:54 +0000

    routing-2

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import ProductList from "./components/ProductList";
+import ProductList from "./pages/ProductList";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import TaskManager from "./components/TaskManager";
-
+import TaskManager from "./pages/TaskManager";
+import NotFound from "./pages/NotFound";
+import ProductDetail from "./components/ProductDetail";
 
 function App() {
   return (
-    
     <BrowserRouter>
       <Navbar />
 
       <Routes>
-
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/product-list/:productId" element={<ProductDetail />} />
         <Route path="/task-manager" element={<TaskManager />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
